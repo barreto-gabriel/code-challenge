37e53d2a66c2519e0d09964d8db51f29572a0c98
Gabriel Barreto <gabrielbarreto@MacBook-Pro.local>
2021-03-13 16:23:47 -0300

    Test temperature table content rendering

--- client/src/components/TemperatureTable.test.js
+++ client/src/components/TemperatureTable.test.js
@@ -2,17 +2,28 @@ import {render, screen} from '@testing-library/react';
 import TemperatureTable from './TemperatureTable';
 
 describe('<TemperatureTable />', () => {
-     it('renders too low', () => {
-        let fakeItems = [
-            {
-                id: "2",
-                name: "IPA",
-                minimumTemperature: 4,
-                maximumTemperature: 6,
-                temperature: 2
-            }
-        ]
 
+     let fakeItems = [
+             {
+                 id: "2",
+                 name: "IPA",
+                 minimumTemperature: 4,
+                 maximumTemperature: 6,
+                 temperature: 2
+             }
+         ]
+
+    it('renders all product details', () => {
+            render(<TemperatureTable items={fakeItems}/>)
+            expect(screen.getByText('Product')).toBeTruthy()
+            expect(screen.getByText('IPA')).toBeTruthy()
+            expect(screen.getByText('Temperature')).toBeTruthy()
+            expect(screen.getByText('2')).toBeTruthy()
+            expect(screen.getByText('Status')).toBeTruthy()
+            expect(screen.getByText('too low')).toBeTruthy()
+        });
+
+     it('renders too low', () => {
         render(<TemperatureTable items={fakeItems}/>)
         expect(screen.getByText('too low')).toBeTruthy()
     });
